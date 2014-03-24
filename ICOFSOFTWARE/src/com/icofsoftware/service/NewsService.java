1858ebfa395f1ca0a8c2eca01b4ef5d849189864
lgqandwrq <lgqandwrq@163.com>
2014-03-24 14:53:42 +0800

    update by lgq

--- ICOFSOFTWARE/src/com/icofsoftware/service/NewsService.java
+++ ICOFSOFTWARE/src/com/icofsoftware/service/NewsService.java
@@ -2,6 +2,7 @@ package com.icofsoftware.service;
 
 import java.sql.ResultSet;
 import java.util.ArrayList;
+import java.util.List;
 
 import com.icofsoftware.beans.NewsBean;
 import com.icofsoftware.db.DBHelper;
@@ -14,7 +15,6 @@ public class NewsService {
 		DBHelper db = new DBHelper();
 		ResultSet rs = null;
 		try {
-			//db.getConnection();
 			rs = db.executeQuery(sql);
 			while (rs.next()) {
 				nb.setNid(rs.getInt("nid"));
@@ -32,5 +32,32 @@ public class NewsService {
 			db.free(rs);
 		}
 		return nb;
+	}
+	
+	public List<NewsBean> getMSENews(){
+		List<NewsBean> nblist = new ArrayList<NewsBean>();
+		NewsBean nb = new NewsBean();
+		String sql = "select a.nid nid,a.title title,a.content content,a.postdate postdate,a.type type,b.username username from news a, admins b where type = 2 and a.posterid = b.aid";
+		DBHelper db = new DBHelper();
+		ResultSet rs = null;
+		try {
+			rs = db.executeQuery(sql);
+			while (rs.next()) {
+				nb.setNid(rs.getInt("nid"));
+				nb.setTitle(rs.getString("title"));
+				nb.setContent(rs.getString("content"));
+				nb.setPostdate(rs.getDate("postdate").toString());
+				nb.setUsername(rs.getString("username"));
+				nb.setType(rs.getInt("type"));
+				
+				System.out.print("~~getSchoolinfo~~"+nb.getTitle()+nb.getContent()+nb.getPostdate()+nb.getType());
+				nblist.add(nb);
+			}
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			db.free(rs);
+		}
+		return nblist;
 	}
 }
